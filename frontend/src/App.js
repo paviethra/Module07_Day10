1e859612e92f18787843faa2f242b30672ba46e3
paviethra <62457057+paviethra@users.noreply.github.com>
2022-10-29 21:00:23 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,55 @@
+
+import React from "react";
+import { Nav, Navbar, Container, Row, Col } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import CreateStudent from "./components/create-student";
+import EditStudent from "./components/edit-student";
+import StudentList from "./components/student-list";
+
+
+export default function App() {
+return (
+	<Router>
+	<div className="App">
+		<header className="App-header">
+		<Navbar bg="dark" variant="dark">
+			<Container>
+			<Navbar.Brand>
+				<Link to={"/create-student"}className="nav-link"> Student Database </Link>
+			</Navbar.Brand>
+
+			<Nav className="justify-content-end">
+				<Nav>
+				<Link to={"/create-student"} className="nav-link"> Create Student </Link>
+				</Nav> 
+				
+				<Nav>
+				<Link to={"/student-list"} className="nav-link"> Student List </Link>
+				</Nav>
+			</Nav>
+			</Container>
+		</Navbar>
+		</header>
+
+		<Container>
+		<Row>
+			<Col md={12}>
+			<div className="wrapper">
+				<Routes>
+				<Route path="/" element={<CreateStudent/>} />
+				<Route path="/create-student" element={<CreateStudent/>} />
+				<Route path="/edit-student/:id" element={<EditStudent/>} />
+				<Route path="/student-list" element={<StudentList/>} />
+				</Routes>
+			</div>
+			</Col>
+		</Row>
+		</Container>
+	</div>
+	</Router>
+);
+};
+
+
